4ea548d196ae839d5e868301393db54c23c1e0b4
Alex Liebhaber <alexliebhaber1992@gmail.com>
2023-03-10 09:44:53 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Static
+app.use(express.static("public"));
+
+// EJS Templates
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+// Home Page
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
